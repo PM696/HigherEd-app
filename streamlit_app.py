7f4df9eb7115ba30c54b30956ba882d47c42578d
PM696 <145720634+PM696@users.noreply.github.com>
2024-08-02 01:02:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("🎈 My new app")
+st.title("HigherEd Institution Class app")
+streamlit run HigherEd.py
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
